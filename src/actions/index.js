4dce3a85dbdd4410415fdaffed3f81609b6ae6f5
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-08 12:47:16 -0500

    Redux-Promise Continued.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,16 @@
 import Axios from 'axios';
 
 const API_KEY = '4be2f28af56d9f3e4d758f04b18e6cb7';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  // const URL = `${ROOT_URL}&q=${city},us`;
-  const URL = `https://samples.openweathermap.org/data/2.5/forecast?q=${city},us&appid=${API_KEY}`;
+  const URL = `${ROOT_URL}&q=${city},us`;
   const request = Axios.get(URL);
 
   return {
     type: FETCH_WEATHER,
-    paylos: request
+    payload: request
   };
 }
