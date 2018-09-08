94712bebafdb6a80f046582cd8f3d354aa1f9133
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-08 12:00:23 -0500

    Ajax Requests with Axios.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import Axios from 'axios';
+
 const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const URL = `${ROOT_URL}&${city},us`;
+  const request = Axios.get(URL);
+
+  return {
+    type: FETCH_WEATHER,
+    paylos: request
+  };
+}
