a24e4fb7f57db5530604b91f89b912b1e8192705
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-08 13:40:41 -0500

    Building a List Container.

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
+
 
 export default class App extends Component {
   
@@ -8,6 +10,7 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
